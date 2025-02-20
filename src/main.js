3bad3e895d52b948dbaa326fe4c28d9de06faf28
GaoYeGithub <152664000+GaoYeGithub@users.noreply.github.com>
2025-02-19 22:22:08 -0500

    Movement, image, sound effects

--- src/main.js
+++ src/main.js
@@ -5,7 +5,58 @@ const k = kaplay();
 
 k.loadRoot("./"); // A good idea for Itch.io publishing later
 k.loadSprite("bean", "sprites/bean.png");
+k.loadSprite("sock", "sprites/sock.png");
+k.loadSound("boom", "sounds/boom.mp3");
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
+const sockObj = k.add([k.pos(120, 80), k.sprite("sock")]);
 
-k.onClick(() => k.addKaboom(k.mousePos()));
+const speed = 200;
+let velocity = { x: 0, y: 0 };
+
+window.addEventListener("keydown", (e) => {
+  switch(e.key.toLowerCase()){
+    case "w":
+      velocity.y = -speed;
+      break;
+    case "s":
+      velocity.y = speed;
+      break;
+    case "a":
+      velocity.x = -speed;
+      break;
+    case "d":
+      velocity.x = speed;
+      break;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  switch(e.key.toLowerCase()){
+    case "w":
+    case "s":
+      velocity.y = 0;
+      break;
+    case "a":
+    case "d":
+      velocity.x = 0;
+      break;
+  }
+});
+
+let lastTime = performance.now();
+function update() {
+  const now = performance.now();
+  const dt = (now - lastTime) / 1000;
+  lastTime = now;
+  
+  sockObj.pos.x += velocity.x * dt;
+  sockObj.pos.y += velocity.y * dt;
+  
+  requestAnimationFrame(update);
+}
+requestAnimationFrame(update);
+
+k.onClick(() => {
+  k.play("boom");
+  k.addKaboom(k.mousePos());
+});
