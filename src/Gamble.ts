e1179db17b77d5ec78de2d18f912ca0ab9974096
Ryan <ryanbrandtstem@gmail.com>
2025-02-21 19:45:00 -0500

    gambling code outline empty functions and comments

---
+++ src/Gamble.ts
@@ -0,0 +1,30 @@
+//gambling code
+//I think this is the rarities the stuff and the things and the stuff
+//i dont get this language at all
+var prize;
+Wash();
+console.log("meow");
+//main fucntion
+function Wash()
+{
+    var randomint = randi(0,100);
+    console.log(randomint);
+}
+//select upgrade function
+function choseUpgrade()
+{
+    //i forgot to download all the things ineed to downlaod to do the htingy for the thingy and the stuff
+
+}
+//buy upgrade
+function buyUpgrade()
+{
+
+}
+//give reward
+function cashOut(prize)
+{
+
+}
+//i think im out of functions to add?
+//i dont understand this language i need to watch a video for the stuff and the things
